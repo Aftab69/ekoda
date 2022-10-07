60d3bf7d30255b093dcf0c4f4f50b6feb359aad3
Aftab69 <aftabhssn3@gmail.com>
2022-10-07 11:34:50 +0530

    loading screen added

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Home.css"
 import showreel from "./Images/Showreel.mp4"
+import loadinggif from "./Images/loading.gif"
 
 const Home = () => {
+  const [ loading, setLoading ] = useState(true)
+  const loadingfunc = () =>{
+    setLoading(false)
+    enableScrolling()
+  }
+  useEffect(()=>{
+    setTimeout( loadingfunc , 4000)
+    disableScrolling()
+    // eslint-disable-next-line
+  },[])
+  const disableScrolling = () =>{
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+  const enableScrolling = () =>{
+    window.onscroll=function(){};
+}
   return (
     <>
-    <div className='homepageContainer'>
-      <video src={showreel} autoPlay loop muted />
-    </div>
+    { loading === false ? 
+       <div className='homepageContainer'>
+        <video src={showreel} autoPlay loop muted />
+       </div> :
+       <></>
+    }
+    
+    { loading === true ?
+      <div className='loadingpageContainer'>
+          <p>LOADING ...</p>
+          <img src={loadinggif} alt={loadinggif} />
+      </div> :
+      <></>
+    }
     </>
   )
 }
