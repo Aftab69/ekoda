709dae88a27b758ddc95b1fb7b21ffe289de2440
Aftab69 <aftabhssn3@gmail.com>
2022-10-07 13:47:03 +0530

    loading screen optimised

--- src/Home.js
+++ src/Home.js
@@ -4,34 +4,31 @@ import showreel from "./Images/Showreel.mp4"
 import loadinggif from "./Images/loading.gif"
 
 const Home = () => {
-  const [ loading, setLoading ] = useState(true)
-  const loadingfunc = () =>{
-    setLoading(false)
-    enableScrolling()
-  }
-  useEffect(()=>{
-    setTimeout( loadingfunc , 4000)
-    disableScrolling()
-    // eslint-disable-next-line
-  },[])
   const disableScrolling = () =>{
     var x=window.scrollX;
     var y=window.scrollY;
     window.onscroll=function(){window.scrollTo(x, y);};
 }
-
   const enableScrolling = () =>{
     window.onscroll=function(){};
 }
+  const [ loading, setLoading ] = useState(true)
+  const loadingfunc = () =>{
+    setLoading(false)
+    enableScrolling()
+    document.body.style.overflowY = "scroll";
+  }
+  useEffect(()=>{
+    document.body.style.overflowY = "hidden";
+    disableScrolling()
+    setTimeout( loadingfunc , 1000)
+    // eslint-disable-next-line
+  },[])
   return (
     <>
-    { loading === false ? 
        <div className='homepageContainer'>
         <video src={showreel} autoPlay loop muted />
-       </div> :
-       <></>
-    }
-    
+       </div>
     { loading === true ?
       <div className='loadingpageContainer'>
           <p>LOADING ...</p>
