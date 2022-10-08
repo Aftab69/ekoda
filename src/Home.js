9bd21057375bded26687ad6f00c9010072c0ce66
Aftab69 <aftabhssn3@gmail.com>
2022-10-08 12:48:11 +0530

    optimised video fro mobile

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import "./Home.css"
+import useMediaQuery from "./useMediaQuery";
 import showreel from "./Images/Showreel.mp4"
+import showreelmobile from "./Images/showreelmobile.mp4"
 import loadinggif from "./Images/loading.gif"
 
 const Home = () => {
+
+  const matches = useMediaQuery("(min-width: 769px)");
+
   const disableScrolling = () =>{
     var x=window.scrollX;
     var y=window.scrollY;
@@ -26,9 +31,15 @@ const Home = () => {
   },[])
   return (
     <>
-       <div id='homepage' className='homepageContainer'>
-        <video src={showreel} autoPlay loop muted />
-       </div>
+    {(matches) ?
+      <div id='homepage' className='homepageContainer'>
+      <video src={showreel} autoPlay loop muted />
+     </div> :
+      <div id='homepage' className='homepageContainer'>
+      <video src={showreelmobile} autoPlay loop muted />
+      </div>
+    }
+       
     { loading === true ?
       <div className='loadingpageContainer'>
           <p>LOADING ...</p>
