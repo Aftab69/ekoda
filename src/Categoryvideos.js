57e3a5bbb9e4c0c9f7e84013f5662631f29d492d
Aftab69 <aftabhssn3@gmail.com>
2022-10-10 16:10:47 +0530

    projects page optimised

---
+++ src/Categoryvideos.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import "./Categoryvideos.css"
+import ReactPlayer from 'react-player/youtube'
+import Zoom from 'react-reveal/Zoom';
+import { Link } from 'react-router-dom';
+import logo from "./Images/logo.png"
+
+const Musicvideos = (props) => {
+  const [ youtubeUrls, setyoutubeUrls ] = useState([])
+  const [ heading, setHeading ] = useState("")
+  useEffect(()=>(
+    // eslint-disable-next-line
+    setyoutubeUrls(props.data),
+    // eslint-disable-next-line
+    setHeading(props.pageheading),
+    // eslint-disable-next-line
+    window. scrollTo(0, 0)
+  ),[props])
+  return (
+    <>
+    <div className='categoryvideosBackground'>
+      <div className='categoryvideosHeading'>
+        <Link to="/"><img className='logoImage' src={logo} alt={logo}/></Link>
+        <p>{heading}</p>
+      </div>
+      <div className='filterContainer'>
+        <Link to="/musicvideos"><button>MUSIC VIDEOS</button></Link>
+        <Link to="/adfilms"><button>AD FILMS</button></Link>
+        <Link to="/fashionfilms"><button>FASHION FILMS</button></Link>
+        <Link to="/shortdocumentarytravelfilms"><button>SHORT / DOCUMENTARY / TRAVEL FILMS</button></Link>
+        <Link to="/behindthescenes"><button>BEHIND THE SCENES</button></Link>
+      </div>
+      <div className='categoryvideosContent'>
+        {youtubeUrls.map((each)=>(
+          <Zoom><ReactPlayer className='eachCategoryVideo' url={each} controls width="768px" height="580px" /></Zoom>
+        ))}
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Musicvideos
--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import emailjs from "@emailjs/browser";
 import "./Contact.css"
+import { Zoom } from 'react-reveal';
 import icon1 from "./Images/youtubeicon.png"
 import icon2 from "./Images/whatsappicon.png"
 import icon3 from "./Images/facebookicon.png"
@@ -28,18 +29,20 @@ const Contact = () => {
     <>
       <div className='contactpageContainer' id='contact'>
         <div className='contactpageHeading'><p>CONTACT US</p></div>
-        <div className='contactpageForm'>
-          <form ref={form} onSubmit={sendEmail} className='formContainer'>
-              <input type="text" name="user_name" placeholder='Enter your name' required />
-              <input type="email" name="user_email" placeholder='Enter a valid email address' required />
-              <textarea type="text" name="message" placeholder='Enter your message' required />
-              <button type="submit" value="Send">Submit</button>
-          </form>
-        </div>  
+          <Zoom>
+            <div className='contactpageForm'>
+              <form ref={form} onSubmit={sendEmail} className='formContainer'>
+                  <input type="text" name="user_name" placeholder='Enter your name' required />
+                  <input type="email" name="user_email" placeholder='Enter a valid email address' required />
+                  <textarea type="text" name="message" placeholder='Enter your message' required />
+                  <button type="submit" value="Send">Submit</button>
+              </form>
+            </div> 
+          </Zoom> 
         <div className='contactpageFooter'>
           <div className='footerDiv1'>
             <div className='footerDiv1Line1'>
-              <img src={icon6} alt={icon6} />
+              <Zoom><img src={icon6} alt={icon6} /></Zoom>
               <p>Our Location :</p>
             </div>
             <p>House no. 1 (2nd floor), Asomi Path Railway colony road, Hatigarh Chariali Hatigarh, Guwahati, Assam</p>
@@ -47,11 +50,11 @@ const Contact = () => {
           <div className='footerDiv2'>
             <p>Email us / Call us : ekodaproductions@gmail.com / +917002401695</p>
             <div className='footericonImages'>
-              <a href='https://www.youtube.com/channel/UCnWLao6UvXoNSupz5jiDMiA' target='_blank' rel="noreferrer"><img src={icon1} alt={icon1} /></a>
-              <a href='https://wa.me/+917002401695/' target='_blank' rel="noreferrer"><img src={icon2} alt={icon2} /></a>
-              <a href='https://www.facebook.com/ekodaproductions' target='_blank' rel="noreferrer"><img src={icon3} alt={icon3} /></a>
-              <a href='https://www.instagram.com/ekodaproductions?igshid=YmMyMTA2M2Y=' target='_blank' rel="noreferrer"><img src={icon4} alt={icon4} /></a>
-              <a href='https://www.linkedin.com/in/ekoda-productions-249488218/' target='_blank' rel="noreferrer"><img src={icon5} alt={icon5} /></a>
+              <a href='https://www.youtube.com/channel/UCnWLao6UvXoNSupz5jiDMiA' target='_blank' rel="noreferrer"><Zoom><img src={icon1} alt={icon1} /></Zoom></a>
+              <a href='https://wa.me/+917002401695/' target='_blank' rel="noreferrer"><Zoom><img src={icon2} alt={icon2} /></Zoom></a>
+              <a href='https://www.facebook.com/ekodaproductions' target='_blank' rel="noreferrer"><Zoom><img src={icon3} alt={icon3} /></Zoom></a>
+              <a href='https://www.instagram.com/ekodaproductions?igshid=YmMyMTA2M2Y=' target='_blank' rel="noreferrer"><Zoom><img src={icon4} alt={icon4} /></Zoom></a>
+              <a href='https://www.linkedin.com/in/ekoda-productions-249488218/' target='_blank' rel="noreferrer"><Zoom><img src={icon5} alt={icon5} /></Zoom></a>
             </div>
           </div>
         </div>
