e33e627bd4c649a54ca8dfc31107325c5368ce05
Aftab69 <aftabhssn3@gmail.com>
2022-10-13 21:13:30 +0530

    react player optimised for mobile

--- src/Projectsmobile.js
+++ src/Projectsmobile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Projectsmobile.css"
-import ReactPlayer from "react-player/youtube"
+import ReactPlayer from "react-player"
 import useMediaQuery from "./useMediaQuery";
 import Zoom from 'react-reveal/Zoom';
 
@@ -18,14 +18,12 @@ const Projectsmobile = () => {
           <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=-bpQpsO7mWs' controls width="480px" height="360px" /></Zoom>
           <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=qWxFXk5CWJQ' controls width="480px" height="360px"/></Zoom>          
           <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=s2xr1_Pr_6U' controls width="480px" height="360px"/></Zoom>          
-          <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=IASy9tKBzlw' controls width="480px" height="360px"/></Zoom>          
           </>
           :
           <>
           <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=-bpQpsO7mWs' controls width="320px" height="200px" /></Zoom>
           <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=qWxFXk5CWJQ' controls width="320px" height="200px"/></Zoom>          
-          <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=s2xr1_Pr_6U' controls width="320px" height="200px"/></Zoom>          
-          <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=IASy9tKBzlw' controls width="320px" height="200px"/> </Zoom>
+          <Zoom><ReactPlayer className='eachVideoMobile' url='https://www.youtube.com/watch?v=s2xr1_Pr_6U' controls width="320px" height="200px"/></Zoom>
           </>
          }
          <a href='https://www.youtube.com/channel/UCnWLao6UvXoNSupz5jiDMiA' target="_blank" rel="noreferrer"><button>View More</button></a>
