1ac5f54c19b2a3f5378900620c7971768744fa53
Aftab69 <aftabhssn3@gmail.com>
2022-11-06 21:00:55 +0530

    before snapshotify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <>
       <Helmet>
         <title>Ekoda Productions</title>
-        <meta name="description" content="EKODA is a Guwahati-based Production House. We are the creators who love to tell stories through visual medium. Our services include video production like music videos, short films, documentaries, event videos, TV commercials, wedding films, photoshoots, logo designs, poster designs and artworks." />
+        <meta name="description" content="EKODA is a Guwahati-Based Production House. We are the creators who love to tell stories through visual medium. Our services include video production like music videos, short films, documentaries, event videos, TV commercials, wedding films, photoshoots, logo designs, poster designs and artworks." />
         <meta name="keywords" content="ekoda, ekoda productions, production house, film making, documentaries, music video shoots, ad shoots, cinematography, script writing" />
       </Helmet>
       <Navbar />
