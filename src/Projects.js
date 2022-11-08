d1c0f805bab65e6d0d580df68711a47d56b7e382
Aftab69 <aftabhssn3@gmail.com>
2022-11-09 02:00:21 +0530

    removed react youtube and added sitemap

--- src/Projects.js
+++ src/Projects.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import "./Projects.css"
-import Youtube from "react-youtube";
+import ReactPlayer from "react-player"
 import Zoom from 'react-reveal/Zoom';
 import { Link } from 'react-router-dom';
 
 const Projects = (props) => {
-  const opts = {
-    height: '480',
-    width: '720',
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
-    },
-  };
   
   return (
     <>
@@ -24,39 +16,39 @@ const Projects = (props) => {
             <div className='videoCategoryHeading'><p>Music Videos :</p></div>
             <div className='videosOuterContainer'>
               <div className='videosInnerContainer' id='videosofbutton1'>
-                  <Zoom><Youtube className='eachVideo' videoId='-bpQpsO7mWs' opts={opts} /></Zoom>
-                  <Zoom><Youtube className='eachVideo' videoId='POuwAy2JTLk' opts={opts} /></Zoom>
+                  <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=-bpQpsO7mWs' controls width="720px" height="480px" /></Zoom>
+                  <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=POuwAy2JTLk' controls width="720px" height="480px" /></Zoom>
                 </div>
                 <Link style={{textDecoration:"none"}} to="/musicvideos" target="_blank"><button name="button1" id="button1" style={{display:"block"}}>View more</button></Link>
             </div>
             <div className='videoCategoryHeading'><p>Ad Films :</p></div>
             <div className='videosOuterContainer'>
               <div className='videosInnerContainer' id='videosofbutton2'>
-                <Zoom><Youtube className='eachVideo' videoId='qWxFXk5CWJQ' opts={opts} /></Zoom>
-                <Zoom><Youtube className='eachVideo' videoId='BBItkycabOM' opts={opts} /></Zoom>
+                <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=qWxFXk5CWJQ' controls width="720px" height="480px" /></Zoom>
+                <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=BBItkycabOM' controls width="720px" height="480px" /></Zoom>
               </div>
               <Link style={{textDecoration:"none"}} to="/adfilms" target="_blank"><button name="button2" id="button2">View more</button></Link>
             </div>
             <div className='videoCategoryHeading'><p>Fashion Films :</p></div>
             <div className='videosOuterContainer'>
               <div className='videosInnerContainer' id='videosofbutton3'>
-                <Zoom><Youtube className='eachVideo' videoId='s2xr1_Pr_6U' opts={opts} /></Zoom>
+                <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=s2xr1_Pr_6U' controls width="720px" height="480px" /></Zoom>
               </div>
               <Link style={{textDecoration:"none"}} to="/fashionfilms" target="_blank"><button name="button3" id="button3">View more</button></Link>
             </div>
             <div className='videoCategoryHeading'><p>Short/Documentary/Travel Films :</p></div>
             <div className='videosOuterContainer'>
               <div className='videosInnerContainer' id='videosofbutton4'>
-                <Zoom><Youtube className='eachVideo' videoId='IASy9tKBzlw' opts={opts} /></Zoom>
-                <Zoom><Youtube className='eachVideo' videoId='_yQ1rMLRpGk' opts={opts} /></Zoom>
+                <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=IASy9tKBzlw' controls width="720px" height="480px" /></Zoom>
+                <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=_yQ1rMLRpGk' controls width="720px" height="480px" /></Zoom>
               </div>
               <Link style={{textDecoration:"none"}} to="/shortdocumentarytravelfilms" target="_blank"><button name="button4" id="button4">View more</button></Link>
             </div>
             <div className='videoCategoryHeading'><p>Behind the Scenes :</p></div>
             <div className='videosOuterContainer'>
               <div className='videosInnerContainer' id='videosofbutton5'>
-                <Zoom><Youtube className='eachVideo' videoId='0bqRgquI-0g' opts={opts} /></Zoom>
-                <Zoom><Youtube className='eachVideo' videoId='H2UnUAGWGak' opts={opts} /></Zoom>
+                <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=0bqRgquI-0g' controls width="720px" height="480px" /></Zoom>
+                <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=H2UnUAGWGak' controls width="720px" height="480px" /></Zoom>
               </div>   
               <Link style={{textDecoration:"none"}} to="/behindthescenes" target="_blank"><button name="button5" id="button5">View more</button></Link>
             </div>      
